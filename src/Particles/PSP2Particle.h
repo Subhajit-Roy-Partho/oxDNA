2ee87db90d43247067b64ef22256fd055febc96d
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2024-06-15 11:45:41 -0700

    update

--- src/Particles/PSP2Particle.h
+++ src/Particles/PSP2Particle.h
@@ -9,6 +9,9 @@ public:
     std::vector<int> iS,iP,neighbours;
     virtual void add_neighbour(BaseParticle *n);
     virtual bool has_bond(BaseParticle *p);
+    virtual bool is_rigid_body(){ //without this a1,a3 won't be updated
+        return true;
+    };
 };
 
 
